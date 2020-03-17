1ff6556919a2c63f9c208f78ed1169d3ad0d2434
Luiz Otávio <luiz-osb@hotmail.com>
2020-03-17 15:00:42 -0300

    Video 1.7

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,31 +1,50 @@
 package br.ce.wcaquino.servicos;
 
 
+import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
 import java.util.Date;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.servicos.LocacaoService;
 import br.ce.wcaquino.utils.DataUtils;
 
-public class LocacaoServiceTest {
+public class LocacaoServiceTest {		
 	
-	@Test
-	public void teste() {
+		@Rule
+		public ErrorCollector error = new ErrorCollector();  
+	
+		@Test
+		public void testeLocacao() {
 		
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Luiz");
-		Filme filme = new Filme("Star Wars", 1, 10.0);
-	
-		Locacao locacao = service.alugarFilme(usuario, filme);
+		Filme filme = new Filme("Star Wars", 0, 10.0);
+	 
+		Locacao locacao;
+		try {
+			locacao = service.alugarFilme(usuario, filme);
+
+			//verificação
+			//verifique q o valor da locação é 5
+			error.checkThat(locacao.getValor(), is(equalTo(6.0)));
+			error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+			error.checkThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(false));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Assert.fail("Não deveria lançar exceção!");
+		}
 		
-		Assert.assertTrue(locacao.getValor() == 10.0);
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		
 	}
 
 }
